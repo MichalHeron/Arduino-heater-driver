a56abe9adfc8717117db085b8f50e202079cbf5f
Heron <michklak91@gmail.com>
2024-04-30 20:46:30 +0200

    ac current valur measure added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Arduino-heater-driver
+program waiting for testing
--- sterowanie_grzalkami.ino
+++ sterowanie_grzalkami.ino
@@ -20,14 +20,37 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // select the pins used on the LCD panel
 #define btnSELECT 4
 #define btnNONE 5
 
-
-#define dryContactNC 1  //from pin x to ground
 bool risePower = true;
 double sensorValue = 0;
 int sensorValueTenTimes;
 double prevSensorValue = 0;
 int prevSensorValueTenTimes;
 int setVoltage = 0;
+int AKUPin = A1;
+const int ACPin = A2;  //set arduino signal current read pin  !!!!!!!do skorygowania@@@@@.   CURR
+int gridNoCurrentValue = 0.3;
+#define ACTectionRange 20;  //set Non-invasive AC Current Sensor tection range (5A,10A,20A)       CURR
+#define VREF 5.0            // check with mutlimetr and calibrate                                 CURR
+
+float readACCurrentValue()  //CURR
+{
+  float ACCurrtntValue = 0;
+  float peakVoltage = 0;
+  float voltageVirtualValue = 0;  //Vrms
+  for (int i = 0; i < 5; i++) {
+    peakVoltage += analogRead(ACPin);  //read peak voltage
+    delay(1);
+  }
+  peakVoltage = peakVoltage / 5;
+  voltageVirtualValue = peakVoltage * 0.707;  //change the peak voltage to the Virtual Value of voltage
+
+  /*The circuit is amplified by 2 times, so it is divided by 2.*/
+  voltageVirtualValue = (voltageVirtualValue / 1024 * VREF) / 2;
+
+  ACCurrtntValue = voltageVirtualValue * ACTectionRange;
+
+  return ACCurrtntValue;
+}
 
 double convertToAkuVoltage(double a) {
   double akuValueVolt;
@@ -58,48 +81,28 @@ void setup() {
   lcd.print("succed ");
   //Set DAC output range
   dac.setDACOutRange(dac.eOutputRange10V);
-
-  // Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
-  // 20K-ohm resistor is pulled to 5V. This configuration causes the input to read
-  // HIGH when the switch is open, and LOW when it is closed.
-  //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(dryContactNC, INPUT_PULLUP);
 }
 
 void loop() {
+  float ACCurrentValue = readACCurrentValue();  //read AC Current Value
   lcd.setCursor(7, 0);
   lcd.print("set:");
   lcd.print(setVoltage);
-  lcd.print(" ");
-  lcd.print(digitalRead(dryContactNC));
   lcd.print("  ");
+  lcd.print(ACCurrentValue);
+  lcd.print("A  ");
 
-  //  if (sensorVal == HIGH) {
-  // if NC C is true then do
   if ((sensorValue > 52)) {
-    // if (sensorValue > 52) {
-    if (risePower) setVoltage += 1;  //Podnieś  o 1%
-    else setVoltage -= 1;            //obniż o 1%
+    if (risePower) setVoltage += 1;  //rise by 1%
+    else setVoltage -= 1;            //low by 1%
     if (prevSensorValue - 0.15 < sensorValue) risePower = true;
     else risePower = false;
     prevSensorValue = sensorValue;
   }
 
-
-  // if (digitalRead(dryContactNC)) { test X
-  //   // if (sensorValue > 52) {
-  //   if (risePower) setVoltage += 1;  //Podnieś  o 1%
-  //   else setVoltage -= 1;            //obniż o 1%
-  //   if (prevSensorValueTenTimes - 1 < sensorValueTenTimes) risePower = true;
-  //   else risePower = false;
-  //   prevSensorValue = sensorValue;
-  // }
-
-  //netstat –nr | grep defaultif (!digitalRead(dryContactNC)) setVoltage = 0;
-
-  if (sensorValue < 53.5) setVoltage -= 5;  //jezeli spadnie ponizej napiecia floating (chyba)
-                                            // if (sensorValue < 50) setVoltage = 0; //wyzerwoanie mocy po naglym spadku ponizej wartosci
-
+  if (sensorValue < 53.5) setVoltage -= 5;                  //jezeli spadnie ponizej napiecia floating (chyba) if drop below floating value
+                                                            // if (sensorValue < 50) setVoltage = 0; //wyzerwoanie mocy po naglym spadku ponizej wartosci
+  if (ACCurrentValue > gridNoCurrentValue) setVoltage = 0;  //if true it means that inverter uses grid power
   if (setVoltage > 100) setVoltage = 100;
   if (setVoltage < 0) setVoltage = 0;
 
@@ -108,14 +111,85 @@ void loop() {
   delay(100);
   dac.store();
 
-  sensorValue = convertToAkuVoltage(analogRead(A1));
+  sensorValue = convertToAkuVoltage(analogRead(AKUPin));
   lcd.setCursor(0, 1);
   lcd.print(sensorValue);
   lcd.print("V");
   lcd.print("      ");
   delay(200);  //Czekaj 1s w sumie
-
-  prevSensorValueTenTimes = prevSensorValue * 10; //test X
-  sensorValueTenTimes = sensorValue * 10;
-
 }
+
+
+// example
+
+// /*!
+//  * @file readACCurrent.
+//  * @n This example reads Analog AC Current Sensor.
+
+//  * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (https://www.dfrobot.com)
+//  * @licence     The MIT License (MIT)
+//  * @get from https://www.dfrobot.com
+
+//  Created 2016-3-10
+//  By berinie Chen <bernie.chen@dfrobot.com>
+
+//  Revised 2019-8-6
+//  By Henry Zhao<henry.zhao@dfrobot.com>
+// */
+
+// const int ACPin = A2;         //set arduino signal read pin
+// #define ACTectionRange 20;    //set Non-invasive AC Current Sensor tection range (5A,10A,20A)
+
+// // VREF: Analog reference
+// // For Arduino UNO, Leonardo and mega2560, etc. change VREF to 5
+// // For Arduino Zero, Due, MKR Family, ESP32, etc. 3V3 controllers, change VREF to 3.3
+// #define VREF 5.0
+
+// float readACCurrentValue()
+// {
+//   float ACCurrtntValue = 0;
+//   float peakVoltage = 0;
+//   float voltageVirtualValue = 0;  //Vrms
+//   for (int i = 0; i < 5; i++)
+//   {
+//     peakVoltage += analogRead(ACPin);   //read peak voltage
+//     delay(1);
+//   }
+//   peakVoltage = peakVoltage / 5;
+//   voltageVirtualValue = peakVoltage * 0.707;    //change the peak voltage to the Virtual Value of voltage
+
+//   /*The circuit is amplified by 2 times, so it is divided by 2.*/
+//   voltageVirtualValue = (voltageVirtualValue / 1024 * VREF ) / 2;
+
+//   ACCurrtntValue = voltageVirtualValue * ACTectionRange;
+
+//   return ACCurrtntValue;
+// }
+
+// void setup()
+// {
+//   Serial.begin(115200);
+//   pinMode(13, OUTPUT);
+
+// }
+
+// void loop()
+// {
+//   float ACCurrentValue = readACCurrentValue(); //read AC Current Value
+//   Serial.print(ACCurrentValue);
+//   Serial.println(" A");
+//   digitalWrite(13, HIGH);
+//   delay(500);
+//   digitalWrite(13, LOW);
+//   delay(500);
+// }
+
+
+
+
+
+// Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
+// 20K-ohm resistor is pulled to 5V. This configuration causes the input to read
+// HIGH when the switch is open, and LOW when it is closed.
+//configure pin 2 as an input and enable the internal pull-up resistor
+//pinMode(dryContactNC, INPUT_PULLUP);
